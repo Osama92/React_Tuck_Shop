107e79e15dd7bb14d17d5add0b6298f06f80671d
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-25 12:43:36 +0100

    Working on Login page

--- src/Login.js
+++ src/Login.js
@@ -3,13 +3,16 @@ import "./styles.css";
 export default function Login() {
   return (
     <div className="Main">
-      <div className="PZ-Logo"></div>
       <div className="Login-Header">
         <p>Hello there! 👋🏾</p>
-        <h1>Welcome to PZC Tuck shop</h1>
+        <h1>Welcome to PZC Tuck shop.</h1>
       </div>
-      <div className="Login-Inputs-Holder"></div>
-      <div className="Login-Inputs-Holder"></div>
+      <div className="Login-Inputs-Holder">
+        <input placeholder="Enter Your Clock Number" />
+      </div>
+      <div className="Login-Inputs-Holder">
+        <input placeholder="Enter Voucher Number" />
+      </div>
       <div className="Login-Modes">
         <div className="Login-Mode"></div>
         <div className="Guest-Mode"></div>
