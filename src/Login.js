c971d54a6439316c8fd1d1bbf102cdbd6348a44d
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-25 13:19:53 +0100

    Working on Login Page

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,22 @@
 import "./styles.css";
 
 export default function Login() {
+  const _check = () => {
+    console.log("Hello");
+  };
   return (
     <div className="Main">
       <div className="Login-Header">
         <p>Hello there! 👋🏾</p>
-        <h1>Welcome to PZC Tuck shop.</h1>
+        <h1>
+          Welcome to <span className="pzc-color">PZC</span> Tuck shop.
+        </h1>
       </div>
       <div className="Login-Inputs-Holder">
         <input placeholder="Enter Your Clock Number" />
       </div>
       <div className="Login-Inputs-Holder">
-        <input placeholder="Enter Voucher Number" />
+        <input placeholder="Enter Voucher Number" onChange={() => _check()} />
       </div>
       <div className="Login-Modes">
         <div className="Login-Mode">
