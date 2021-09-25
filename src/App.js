ff755a0340b669f003a3cad57ae1278a3d238a0c
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-25 16:17:33 +0100

    Working on Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
 import Login from "./Login.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
 
 export default function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Login />
+      </div>
+      <Switch>
+        <Route path="/" component={Login}></Route>
+        <Route path="/home" component={Home}></Route>
+      </Switch>
+    </Router>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,4 @@
 import "./styles.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./Home";
 
 export default function Login() {
   //Variables
@@ -21,48 +19,40 @@ export default function Login() {
     console.log(voucherNumber);
   };
   function Mynav() {
-    return <Link to="/home"></Link>;
+    // return <Link to="/home"></Link>;
     // console.log("HDH");
   }
 
   return (
-    <Router>
-      <div className="Main">
-        <div className="Login-Header">
-          <p>Hello there! 👋🏾</p>
-          <h1>
-            Welcome to <span className="pzc-color">PZC</span> Tuck shop.
-          </h1>
+    <div className="Main">
+      <div className="Login-Header">
+        <p>Hello there! 👋🏾</p>
+        <h1>
+          Welcome to <span className="pzc-color">PZC</span> Tuck shop.
+        </h1>
+      </div>
+      <div className="Login-Inputs-Holder">
+        <input
+          placeholder="Enter Your Clock Number"
+          onChange={(e) => _get_clock_number(e.target.value)}
+        />
+      </div>
+      <div className="Login-Inputs-Holder">
+        <input
+          placeholder="Enter Voucher Number"
+          onChange={(e) => _get_voucher_number(e.target.value)}
+        />
+      </div>
+      <div className="Login-Modes">
+        <div className="Login-Mode" onClick={() => Mynav()}>
+          <p>
+            Log in <span> › </span>
+          </p>
         </div>
-        <div className="Login-Inputs-Holder">
-          <input
-            placeholder="Enter Your Clock Number"
-            onChange={(e) => _get_clock_number(e.target.value)}
-          />
-        </div>
-        <div className="Login-Inputs-Holder">
-          <input
-            placeholder="Enter Voucher Number"
-            onChange={(e) => _get_voucher_number(e.target.value)}
-          />
-        </div>
-        <div className="Login-Modes">
-          <div className="Login-Mode" onClick={() => Mynav()}>
-            <p>
-              Log in <span> › </span>
-            </p>
-          </div>
-          <div className="Guest-Mode">
-            <p>Guest Mode</p>
-          </div>
+        <div className="Guest-Mode">
+          <p>Guest Mode</p>
         </div>
       </div>
-
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    </div>
   );
 }
