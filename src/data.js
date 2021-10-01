dad7e52ca56f02f634ce385610c14397b621a534
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-10-01 12:42:32 +0100

    changes

--- src/data.js
+++ src/data.js
@@ -3,35 +3,35 @@ export const products = [
     id: 1,
     name: "Zip 900g",
     price: 200,
-    image: require("../public/products/ZipDet.png").default,
+    image: require("./products/ZipDet.png").default,
     qty: 1
   },
   {
     id: 2,
     name: "Canoe 180g",
     price: 300,
-    image: require("../public/products/CanoeDet.png"),
+    image: require("./products/CanoeDet.png"),
     qty: 1
   },
   {
     id: 3,
     name: "Canoe bar Soap",
     price: 400,
-    image: require("../public/products/CanoeSoap.png"),
+    image: require("./products/CanoeSoap.png"),
     qty: 1
   },
   {
     id: 4,
     name: "Morning Fresh 1Ltr",
     price: 500,
-    image: require("../public/products/MorningFrsh.png"),
+    image: require("./products/MorningFrsh.png"),
     qty: 1
   },
   {
     id: 5,
     name: "Robb Oint 23ml",
     price: 600,
-    image: require("../public/products/Robb23.png"),
+    image: require("./products/Robb23.png"),
     qty: 1
   }
 ];
