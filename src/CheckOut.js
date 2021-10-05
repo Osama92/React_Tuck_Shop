7becf3e8d7d614c92bd0c1251b454ab71cc65d4d
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-10-05 14:40:53 +0100

    Working on Check out Screen

--- src/CheckOut.js
+++ src/CheckOut.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class CheckOut extends Component {
   constructor(props) {
@@ -10,7 +11,23 @@ class CheckOut extends Component {
   }
 
   render() {
-    return <p>Check Out Page</p>;
+    const back_btn = "bx bx-arrow-back";
+
+    return (
+      <div className="Main">
+        <div className="Cart-Header">
+          <p>Hey there! 👋🏾</p>
+          <h1>
+            Welcome to your <span className="pzc-color">Shopping Cart</span>.
+          </h1>
+        </div>
+        <Link to={"/cart"}>
+          <div className="Make-Order">
+            <i className={back_btn}></i>
+          </div>
+        </Link>
+      </div>
+    )
   }
 }
 
