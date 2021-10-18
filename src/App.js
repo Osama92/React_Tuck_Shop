3165eec520d7ed151ab23d6f7477836a012c84d4
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-10-18 13:48:47 +0100

    Removed Auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import Login from "./Login";
+// import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Cart from "./Cart";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Login} exact></Route>
+        {/* <Route path="/" component={Login} exact></Route> */}
         <Route path="/home" component={Home}></Route>
         <Route path="/cart" component={Cart}></Route>
         <Route path="/checkOut" component={CheckOut}></Route>
